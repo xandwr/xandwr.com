65c2f2a89053efd6332ac74c566e2da50cc687bf
Xander <ajamespickering@gmail.com>
2025-07-28 17:24:49 -0600

    Astro rework + basic prototype template

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: "Silent Carriers",
+        description: "A multiplayer horror delivery game with emergent AI threats and physics-based madness.",
+        // github: "",
+        website: "https://silentcarriers.com",
+        status: "PAUSED"
+    },
+    {
+        title: "Stairs for Stacks",
+        description: "A Roblox game where you climb an endless staircase for cash.",
+        // github: "",
+        website: "https://www.roblox.com/games/78165662298720/Stairs-for-Stacks",
+        status: "ACTIVE"
+    },
+];
