ebe44ab8df0f82c021f5da3dc7a604a786ff2c52
Xander <ajamespickering@gmail.com>
2025-07-28 18:45:46 -0600

    site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://xandwr.github.io",
     vite: {
         plugins: [tailwindcss()],
     },
